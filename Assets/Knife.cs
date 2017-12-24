9b369d25ac27dec6f595436a8af2f3347dce09f6
benan01 <34815455+benan01@users.noreply.github.com>
2017-12-24 19:46:45 +0300

    Add files via upload

---
+++ Assets/Knife.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	public float force=5;
+	public float torque=20;
+
+	private Vector3 startSwipe;
+	private Vector3 endSwipe;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			startSwipe =Camera.main.ScreenToViewportPoint(Input.mousePosition);
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			endSwipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+			Swipe ();
+		}
+
+	}
+	void Swipe()
+	{
+		rb.isKinematic = false;
+		Vector2 swipe = endSwipe - startSwipe;
+		rb.AddForce (swipe*force, ForceMode.Impulse);
+		rb.AddTorque (0f,0f,torque,ForceMode.Impulse);
+	}
+	private void OnTriggerEnter()
+	{
+		rb.isKinematic = true;
+
+	}
+}
